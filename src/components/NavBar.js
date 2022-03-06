ab2c2e38c6369fd75029d0204ddd8924c0e06e16
Joshua George <horheyinc8@gmail.com>
2022-03-06 19:10:31 +0100

    neon hover effects added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,21 +8,24 @@ class NavBar extends Component {
         <div className="relative container mx-auto flex justify-between ">
           <nav className="flex">
             <NavLink
+              id="neon"
               exact
               to="/"
               activeClassName="text-white"
-              className="inflex-flex items-center justify-center py-9 px-2 mr-4 rounded text-blue-100 hover:bg-gray-900 text-3xl font-bold cursive tracking-widest md:text-6xl"
+              className=" inflex-flex items-center justify-center rounded text-blue-100 text-3xl font-bold cursive tracking-widest md:text-6xl"
             >
               Home
             </NavLink>
             <NavLink
+              id="neon"
               to="/projects"
               activeClassName="bg-gray-800"
-              className="inline-flex items-center text-3xl py-1 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
+              className="neon inline-flex items-center text-3xl py-1 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
             >
               Projects
             </NavLink>
             <NavLink
+              id="neon"
               to="/about"
               activeClassName="bg-gray-800"
               className="inline-flex items-center text-3xl py-3 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
@@ -30,9 +33,10 @@ class NavBar extends Component {
               About Me
             </NavLink>
             <NavLink
+              id="neon"
               to="/contact"
               activeClassName="bg-gray-800"
-              className="inline-flex items-center text-3xl py-3 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
+              className="neon inline-flex items-center text-3xl py-3 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
             >
               Contact
             </NavLink>
