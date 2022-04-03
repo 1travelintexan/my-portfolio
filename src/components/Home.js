3cd8f1c3a54d282df8dbe430dea2188676f24793
Joshua George <horheyinc8@gmail.com>
2022-04-03 14:00:30 +0200

    inspiration quote added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import image from "../images/mac.jpeg";
 import htmlImage from "../images/html.png";
 import cssImage from "../images/css.png";
@@ -8,73 +8,95 @@ import reactImage from "../images/react.png";
 import tsImage from "../images/ts.png";
 import mongoImage from "../images/mongoDB.png";
 
-class Home extends Component {
-  render() {
-    let randomQuote = "";
-    let author = "";
+function Home() {
+  const [quote, setQuote] = useState({ quote: "", author: "" });
+
+  useEffect(() => {
     const apiget = async () => {
       let fetchResponse = await fetch("https://type.fit/api/quotes");
       let data = await fetchResponse.json();
       let randomQuoteIndex = Math.floor(Math.random() * data.length);
-      randomQuote = data[randomQuoteIndex].text;
-      author = data[randomQuoteIndex].author;
-      console.log("quote new", randomQuote, author);
+
+      setQuote({
+        quote: data[randomQuoteIndex].text,
+        author: data[randomQuoteIndex].author,
+      });
     };
     apiget();
-    return (
-      <main>
-        <img src={image} alt="macbook" className=" background" loading="lazy" />
-        <section className="home">
-          <h5 className="slogan">Do your best and the rest will follow</h5>
-        </section>
-        <div className="logos-container">
-          <div className="logos-div">
-            <img
-              src={jsImage}
-              alt="js"
-              className="logos relative"
-              loading="lazy"
-            />
-            <img
-              src={htmlImage}
-              alt="html"
-              className="logos relative"
-              loading="lazy"
-            />
-            <img
-              src={cssImage}
-              alt="css"
-              className="logos relative"
-              loading="lazy"
-            />
-            <img
-              src={reactImage}
-              alt="react"
-              className="logos relative"
-              loading="lazy"
-            />
-            <img
-              src={tsImage}
-              alt="ts"
-              className="logos relative"
-              loading="lazy"
-            />
-            <img
-              src={nodeImage}
-              alt="node"
-              className="logos relative"
-              loading="lazy"
-            />
-            <img
-              src={mongoImage}
-              alt="mongo"
-              className="logos relative"
-              loading="lazy"
-            />
-          </div>
-        </div>
-      </main>
-    );
+  }, []);
+
+  async function handleQuote() {
+    let fetchResponse = await fetch("https://type.fit/api/quotes");
+    let data = await fetchResponse.json();
+    let randomQuoteIndex = Math.floor(Math.random() * data.length);
+    setQuote({
+      quote: data[randomQuoteIndex].text,
+      author: data[randomQuoteIndex].author,
+    });
   }
+
+  return (
+    <main>
+      <img src={image} alt="macbook" className=" background" loading="lazy" />
+
+      <div className="quote-container">
+        <h1 className="home-banner">Inspirational Quote of the day:</h1>
+        <section className="quote-div">
+          <h5 className="quote">{quote.quote}</h5>
+          <h6>-{quote.author}</h6>
+          <h3 id="neon" onClick={handleQuote}>
+            New Quote
+          </h3>
+        </section>
+      </div>
+      <div className="logos-container">
+        <div className="logos-div">
+          <img
+            src={jsImage}
+            alt="js"
+            className="logos relative"
+            loading="lazy"
+          />
+          <img
+            src={htmlImage}
+            alt="html"
+            className="logos relative"
+            loading="lazy"
+          />
+          <img
+            src={cssImage}
+            alt="css"
+            className="logos relative"
+            loading="lazy"
+          />
+          <img
+            src={reactImage}
+            alt="react"
+            className="logos relative"
+            loading="lazy"
+          />
+          <img
+            src={tsImage}
+            alt="ts"
+            className="logos relative"
+            loading="lazy"
+          />
+          <img
+            src={nodeImage}
+            alt="node"
+            className="logos relative"
+            loading="lazy"
+          />
+          <img
+            src={mongoImage}
+            alt="mongo"
+            className="logos relative"
+            loading="lazy"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
+
 export default Home;
