832f552a90e90d1d45456be696c4d7c32d528f19
Joshua George <horheyinc8@gmail.com>
2022-04-03 21:43:41 +0200

    buid deleted

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 class NavBar extends Component {
@@ -45,46 +45,6 @@ class NavBar extends Component {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      // <header className="nav relative">
-      //   <div className="relative container mx-auto flex justify-between ">
-      //     <nav className="flex">
-      //       <NavLink
-      //         id="neon"
-      //         exact
-      //         to="/"
-      //         activeClassName="text-white"
-      //         className=" inflex-flex items-center justify-center rounded text-blue-100 text-3xl font-bold cursive tracking-widest md:text-6xl"
-      //       >
-      //         Home
-      //       </NavLink>
-      //       <NavLink
-      //         id="neon"
-      //         to="/projects"
-      //         activeClassName="bg-gray-800"
-      //         className="neon inline-flex items-center text-3xl py-1 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
-      //       >
-      //         Projects
-      //       </NavLink>
-      //       <NavLink
-      //         id="neon"
-      //         to="/about"
-      //         activeClassName="bg-gray-800"
-      //         className="inline-flex items-center text-3xl py-3 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
-      //       >
-      //         About Me
-      //       </NavLink>
-      //       <NavLink
-      //         id="neon"
-      //         to="/contact"
-      //         activeClassName="bg-gray-800"
-      //         className="neon inline-flex items-center text-3xl py-3 px-3 my-6 rounded text-blue-100 hover:bg-gray-900"
-      //       >
-      //         Contact
-      //       </NavLink>
-      //     </nav>
-      //   </div>
-      // </header>
     );
   }
 }
