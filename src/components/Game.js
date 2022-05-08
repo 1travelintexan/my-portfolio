3e098e1c0d690abe84c477627dbbd8fb7dbce154
Joshua George <horheyinc8@gmail.com>
2022-05-08 10:45:21 +0200

    images added

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,15 @@ import { useEffect, useRef } from "react";
 import p5 from "p5";
 import background from "../images/houseBG.jpeg";
 import dadImage from "../images/dad.png";
+import babyImage from "../images/baby.png";
+import coffeeTableImage from "../images/coffeeTable.png";
+import puppyImage from "../images/puppy.png";
 
 let bg;
 let dad;
+let baby;
+let coffeeTable;
+let puppy;
 let dadSize = 130;
 let dadY;
 let tableX;
@@ -14,6 +20,9 @@ function sketch(p) {
   p.setup = function () {
     bg = p.loadImage(background);
     dad = p.loadImage(dadImage);
+    baby = p.loadImage(babyImage);
+    coffeeTable = p.loadImage(coffeeTableImage);
+    puppy = p.loadImage(puppyImage);
     p.createCanvas(window.innerWidth, window.innerHeight * 0.35).parent(
       "game-container"
     );
@@ -25,7 +34,7 @@ function sketch(p) {
   p.draw = function () {
     p.background(bg);
     p.image(dad, 50, dadY, dadSize, dadSize);
-    p.rect(tableX, tableY, 200, 50);
+    p.image(coffeeTable, 200, dadY, dadSize, dadSize);
     tableX = tableX - 1;
   };
 }
