fed0d426f8519ab96604189d1ac8379f904279fb
Joshua George <horheyinc8@gmail.com>
2022-06-02 22:31:19 +0200

    styles and collision added

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,30 +8,46 @@ import puppyImage from "../images/puppy.png";
 import coffeeImage from "../images/coffee.png";
 
 let bg;
+//dad image and coordinates
 let dad;
+let dadSize = 160;
+let dadX = 50;
+let dadY;
+let dadJumping;
+//coffee image and coordinates
 let coffee;
+let coffeeSize;
+let coffeeY;
+let coffeeX;
+
 let objectsArray;
 let baby;
 let coffeeTable;
 let puppy;
 let speed;
-let dadSize = 160;
-let dadY;
+
 function sketch(p) {
   // p is a reference to the p5 instance this sketch is attached to
   p.setup = function () {
-    p.createCanvas(window.innerWidth, window.innerHeight * 0.35).parent(
+    p.createCanvas(window.innerWidth - 33, window.innerHeight * 0.35).parent(
       "game-container"
     );
+
     //load all images
     bg = p.loadImage(background);
     dad = p.loadImage(dadImage);
+    dadY = p.height - dadSize;
+    dadJumping = false;
+
     coffee = p.loadImage(coffeeImage);
+    coffeeSize = 175;
+    coffeeX = p.width * 2;
+    coffeeY = p.height - coffeeSize - 30;
+
     baby = p.loadImage(babyImage);
     coffeeTable = p.loadImage(coffeeTableImage);
     puppy = p.loadImage(puppyImage);
     speed = 2;
-    dadY = p.height - dadSize;
 
     objectsArray = [
       {
@@ -60,13 +76,42 @@ function sketch(p) {
 
   p.draw = function () {
     p.background(bg);
-    p.image(dad, 50, dadY, dadSize, dadSize);
-    p.image(coffee, p.width - 200, dadY, 200, 200);
+    p.image(dad, dadX, dadY, dadSize, dadSize);
+    p.image(coffee, coffeeX, coffeeY, coffeeSize, coffeeSize);
+    coffeeX = coffeeX - 1.5;
+
+    if (p.mouseIsPressed) {
+      dadJumping = true;
+    }
+
+    if (dadJumping === true) {
+      dadY = dadY - 4;
+    } else if (dadJumping === false) {
+      dadY = dadY + 6;
+    }
+    if (dadY < p.height - dadSize - 160) {
+      dadJumping = false;
+    }
+    if (dadY >= p.height - dadSize) {
+      dadY = p.height - dadSize;
+    }
+
     for (let i = 0; i < objectsArray.length; i++) {
       let current = objectsArray[i];
       p.image(current.img, current.x, current.y, current.width, current.height);
-      current.x = current.x - 5;
-      if (current.x < -200) {
+      current.x = current.x - 8;
+
+      //collision detection
+      if (
+        current.x < dadX + dadSize - 70 &&
+        dadY + dadSize > current.y + 30 &&
+        dadX < current.x + current.width - 30
+      ) {
+        p.noLoop();
+      }
+
+      // cycle the objects to the front
+      if (current.x < -300) {
         current.x = p.width + 1800;
       }
     }
