696fa89c84e3d05f49002a55219fb99eeb562cdc
Joshua George <horheyinc8@gmail.com>
2023-05-05 11:36:14 +0200

    tic tac tron

--- src/components/ReactProjects.js
+++ src/components/ReactProjects.js
@@ -29,21 +29,21 @@ function ReactProjects() {
               </h5>
             </div>
             <div className="project relative">
-              <a href="https://star-wars-app-beta.vercel.app/">
-                <h3>Star Wars APP (Next.js)</h3>
-              </a>
-              <h5 className="slogan">
-                Using SWAPI ('Star Wars API') to display characters, planets and
-                film details. Built with Next js, deployed with Vercel
-              </h5>
-            </div>
-            <div className="project relative">
               <a href="https://massage-by-solange.netlify.app">
                 <h3> Massage by Solange</h3>
               </a>
               <h5 className="slogan">
                 Static webpage for a friend and amazing masseuse in Ericeira,
                 Portugal
+              </h5>
+            </div>
+            <div className="project relative">
+              <a href="https://tic-tac-tron.netlify.app">
+                <h3>Tic-Tac-Tron</h3>
+              </a>
+              <h5 className="slogan">
+                Tic Tac Toe game with styles inspired by the Tron movie, made
+                with Vite & deployed with Netlify
               </h5>
             </div>
             <div className="project relative">
